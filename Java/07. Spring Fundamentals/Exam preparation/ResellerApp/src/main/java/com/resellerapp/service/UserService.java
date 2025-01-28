8facc5a6b57148d11a66112aaaf3553b54473316
Kiril Kirilov <kirilov@hotmail.co.uk>
2025-01-28 19:36:07 +0000

    re

---
+++ Java/07. Spring Fundamentals/Exam preparation/ResellerApp/src/main/java/com/resellerapp/service/UserService.java
@@ -0,0 +1,10 @@
+package com.resellerapp.service;
+
+import com.resellerapp.model.UserRegisterBindingModel;
+
+public interface UserService {
+
+     void register(UserRegisterBindingModel userRegisterBindingModel);
+
+
+}
